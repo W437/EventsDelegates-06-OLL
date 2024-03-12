752327769703cf1efa422afe6029d6b649dd1c03
V.431_ <waelabdelal7@gmail.com>
2024-03-12 14:52:21 +0200

    Aa!

---
+++ ObservableLimitedList.cs
@@ -0,0 +1,50 @@
+﻿// ---- C# II (Dor Ben Dor) ----
+// Wael Abd Elal
+// -----------------------------
+
+public class ObservableLimitedList(Predicate<string> predicate, uint size)
+{
+    private Predicate<string> _predicate = predicate;
+    private uint _size = size;
+    private List<string> _items = new List<string>();
+
+    public delegate void ListChangedEventHandler(object source, ListChangedEventArgs e);
+    public event ListChangedEventHandler? ListChanged;
+    public IEnumerable<string> GetItems() => _items.AsReadOnly();
+
+
+    public bool TryAdd(string item)
+    {
+        if (!(_items.Count > _size) && _predicate(item))
+        {
+            _items.Add(item);
+            ListChanged?.Invoke(this, new ListChangedEventArgs(item));
+
+            if (_items.Count >= _size)
+            {
+                PrintAll();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public void PrintAll()
+    {
+        Console.WriteLine($"All {_items.Count} list items:");
+        foreach (var item in _items)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+public class ListChangedEventArgs : EventArgs
+{
+    public string ItemChanged { get; }
+
+    public ListChangedEventArgs(string itemChanged)
+    {
+        ItemChanged = itemChanged;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// ---- C# II (Dor Ben Dor) ----
+// Wael Abd Elal
+// -----------------------------
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ObservableLimitedList list = new ObservableLimitedList(item => item.Contains("s") || item.Contains("S"), 10);
+
+        list.ListChanged += (sender, e) =>
+        {
+            var listSender = sender as ObservableLimitedList;
+            if (listSender != null)
+            {
+                var items = listSender.GetItems().ToList();
+                Console.Write($"List changed: ");
+                for (int i = 0; i < items.Count(); i++)
+                {
+                    var item = items[i];
+
+                    if (item == e.ItemChanged)
+                    {
+                        for (int j = 0; j < item.Length; j++)
+                        {
+                            char c = item[j];
+                            // Highlight 's' or 'S'
+                            if (char.ToLower(c) == 's')
+                            {
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.Write(c);
+                                Console.ResetColor();
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write(c);
+                                Console.ResetColor();
+                            }
+                        }
+                        Console.WriteLine(" ");
+                    }
+                    else
+                    {
+                        // default
+                        Console.Write($"{item}, ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        };
+
+        string[] itemsToAdd = { "tokyo", "Tuxedo", "List", "intel", "Doc", "testers", "demo", "array", "Strings", "end" };
+        foreach (var item in itemsToAdd)
+        {
+            list.TryAdd(item);
+        }
+    }
+}
